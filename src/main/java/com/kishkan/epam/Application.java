f0f4abbe124691b2cbdf05a7a5e9e01e29ba5943
Ivan <Kishkan91@gmail.com>
2019-10-04 11:12:29 +0400

    init dev branch

--- src/main/java/com/kishkan/epam/Application.java
+++ src/main/java/com/kishkan/epam/Application.java
@@ -3,7 +3,6 @@ package com.kishkan.epam;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Log: end");
 
     }
 }
---
+++ src/main/java/com/kishkan/epam/dto/Goods.java
@@ -0,0 +1,12 @@
+package com.kishkan.epam.dto;
+
+import java.time.LocalDate;
+
+public class Goods {
+    private long id;
+    private int stock;
+    private String model;
+    private LocalDate ProductionDate;
+    private String color;
+    private Double price;
+}
